cfffa89b5de3626d147c620f49c5522a3ba6c75f
Henry Darana <henry.darana@tray.com>
2023-03-29 15:13:41 +0530

    Misc #15

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -28,7 +28,7 @@ public class LoginTest extends OurDriverManager {
         Utils.sleep(5000);
         driver.rotate(ScreenOrientation.LANDSCAPE);
         Utils.sleep(5000);
-        driver.activateApp("com.vendsy.tray");
+        //driver.activateApp("com.vendsy.tray");
         //driver.activateApp("com.vendsy.tray.pos");
         //driver.switchTo().alert().accept();
 
