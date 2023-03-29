b470faf37f4b20a13aef31c718f76d894d950df7
Henry Darana <henry.darana@tray.com>
2023-03-30 03:01:46 +0530

    Misc #38

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -2,13 +2,10 @@ package tests;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
-import org.openqa.selenium.By;
 import org.openqa.selenium.ScreenOrientation;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
@@ -16,7 +13,6 @@ import utils.Utils;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class LoginTest {
@@ -24,7 +20,7 @@ public class LoginTest {
     public static final String SERVER_NAME = "//android.widget.CheckedTextView[contains(@text, '<serverName>')]";
     public static final String VENUE_NAME = "//android.widget.TextView[contains(@text, '<venueName>')]";
     public static AndroidDriver<MobileElement> driver;
-    WebDriverWait wait;
+    //WebDriverWait wait;
     String hostName = "https://qa.vendsy.com/";
     String kdsUsername = "qa_automation_kds_login_2@gmail.com";
     String kdsPassword = "KDSLogin#2";
@@ -41,7 +37,7 @@ public class LoginTest {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         driver = new AndroidDriver<MobileElement>(url, desiredCapabilities);
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        wait = new WebDriverWait(driver, Duration.ofMillis(30000));
+        //wait = new WebDriverWait(driver, Duration.ofMillis(30000));
     }
 
     @AfterSuite
@@ -63,7 +59,7 @@ public class LoginTest {
 
 
     public LoginTest clickTrayImage() {
-        wait.until(ExpectedConditions.elementToBeClickable(By.id("developer_mode")));
+        //wait.until(ExpectedConditions.elementToBeClickable(By.id("developer_mode")));
         MobileElement imgTray = driver.findElementById("developer_mode");
         for (int i = 1; i <= 5; i++)
             imgTray.click();
@@ -71,7 +67,7 @@ public class LoginTest {
     }
 
     public LoginTest selectServer(String serverName) {
-        wait.until(ExpectedConditions.elementToBeClickable(By.id("android:id/text1")));
+        //wait.until(ExpectedConditions.elementToBeClickable(By.id("android:id/text1")));
         MobileElement drpServers = driver.findElementById("android:id/text1");
         drpServers.click();
         MobileElement e = driver.findElementByXPath(SERVER_NAME.replace("<serverName>", serverName));
@@ -81,14 +77,14 @@ public class LoginTest {
 
 
     public LoginTest clickLoginButton() {
-        wait.until(ExpectedConditions.elementToBeClickable(By.id("loginButton")));
+        //wait.until(ExpectedConditions.elementToBeClickable(By.id("loginButton")));
         MobileElement btnLogin = driver.findElementById("loginButton");
         btnLogin.click();
         return this;
     }
 
     public LoginTest loginToKDS(String username, String password) {
-        wait.until(ExpectedConditions.elementToBeClickable(By.id("submitButton")));
+        // wait.until(ExpectedConditions.elementToBeClickable(By.id("submitButton")));
         MobileElement txtUsername = driver.findElementById("dialog_login_username");
         MobileElement txtPassword = driver.findElementById("dialog_login_password");
         MobileElement btnSubmit = driver.findElementById("submitButton");
@@ -99,14 +95,14 @@ public class LoginTest {
     }
 
     public LoginTest selectVenueInVenueList(String venueName) {
-        Utils.sleep(5000);
+        Utils.sleep(2000);
         MobileElement e = driver.findElementByXPath(VENUE_NAME.replace("<venueName>", venueName));
         e.click();
         return this;
     }
 
     public LoginTest selectDevice(String deviceName) {
-        Utils.sleep(5000);
+        Utils.sleep(2000);
         WebElement selDevices = driver.findElementById("kdsList");
         Select select = new Select(selDevices);
         select.selectByVisibleText(deviceName);
