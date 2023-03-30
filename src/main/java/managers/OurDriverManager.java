c62e51ab85087ce9898bb8452d078b6ae5933b92
Henry Darana <henry.darana@tray.com>
2023-03-31 01:45:29 +0530

    Push after checking on local.

--- src/main/java/managers/OurDriverManager.java
+++ src/main/java/managers/OurDriverManager.java
@@ -1,21 +1,65 @@
 package managers;
 
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
+
 public class OurDriverManager {
 
-//    public static AndroidDriver<MobileElement> driver;
-//
-//    @BeforeSuite
-//    private void setUpAppium() throws MalformedURLException {
-//
-//        final String urlRemote = "http://127.0.0.1:4723/wd/hub";
-//        URL url = new URL(urlRemote);
-//        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-//        driver = new AndroidDriver<MobileElement>(url, desiredCapabilities);
-//        driver.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
-//    }
-//
-//    @AfterSuite(alwaysRun = true)
-//    public void tearDownAppium() {
-//        driver.quit();
-//    }
+    public static final String SERVER_NAME = "//android.widget.CheckedTextView[contains(@text, '<serverName>')]";
+    public static final String VENUE_NAME = "//android.widget.TextView[contains(@text, '<venueName>')]";
+    public static final String DEVICE_NAME = "//android.widget.TextView[contains(@text, '<deviceName>')]";
+
+    public static final String hostName = "https://stage.tray.com/";
+    public static final String kdsUsername = "qa_automation_kds_login_2@gmail.com";
+    public static final String kdsPassword = "KDSLogin#2";
+    public static final String venueName = "Reports Validation - Do Not Use or Delete.";
+    public static final String kdsDevice = "POS4";
+
+    public static AndroidDriver<MobileElement> driver;
+
+    @BeforeSuite
+    public void setupAppium() throws MalformedURLException {
+        final String urlRemote = "http://127.0.0.1:4723/wd/hub";
+        URL url = new URL(urlRemote);
+
+        if (System.getenv("DEVICEFARM_DEVICE_NAME") == null) setUpAppiumOnLocal(url);
+        else setUpAppiumOnDeviceFarm(url);
+    }
+
+    private void setUpAppiumOnDeviceFarm(URL url) {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        driver = new AndroidDriver<MobileElement>(url, desiredCapabilities);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    }
+
+    public void setUpAppiumOnLocal(URL url) {
+        String lnkKDS = System.getProperty("user.dir") + "/src/test/resources/KDS.apk";
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability("udid", "HA0KFKBC");
+        caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "android");
+        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
+        caps.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10");
+        caps.setCapability("autoGrantPermissions", "true");
+        caps.setCapability("appWaitDuration", "5000");
+        caps.setCapability("appWaitPackage", "com.vendsy.tray.pos");
+        caps.setCapability("appWaitActivity", "com.vendsy.tray.pos.InitActivity");
+        caps.setCapability("app", lnkKDS);
+        caps.setCapability(MobileCapabilityType.FULL_RESET, "true");
+        driver = new AndroidDriver(url, caps);
+
+    }
+
+    @AfterSuite
+    public void tearDownAppium() {
+        driver.quit();
+    }
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,65 +1,23 @@
 package tests;
 
 import io.appium.java_client.MobileElement;
-import io.appium.java_client.android.AndroidDriver;
-import org.openqa.selenium.ScreenOrientation;
+import managers.OurDriverManager;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.Select;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import utils.Utils;
 
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.concurrent.TimeUnit;
-
-public class LoginTest {
-
-    public static final String SERVER_NAME = "//android.widget.CheckedTextView[contains(@text, '<serverName>')]";
-    public static final String VENUE_NAME = "//android.widget.TextView[contains(@text, '<venueName>')]";
-    public static AndroidDriver<MobileElement> driver;
-    //WebDriverWait wait;
-    String hostName = "https://qa.vendsy.com/";
-    String kdsUsername = "qa_automation_kds_login_2@gmail.com";
-    String kdsPassword = "KDSLogin#2";
-    String venueName = "Automation_2021";
-    //MobileElement pnlVenues = driver.findElementById("android:id/parentPanel");
-    //By buttonLogin = By.id("com.vendsy.tray.pos:id/loginButton");
-    String kdsDevice = "POS4";
-
-    @BeforeSuite
-    private void setUpAppium() throws MalformedURLException {
-
-        final String urlRemote = "http://127.0.0.1:4723/wd/hub";
-        URL url = new URL(urlRemote);
-        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-        driver = new AndroidDriver<MobileElement>(url, desiredCapabilities);
-        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-        //wait = new WebDriverWait(driver, Duration.ofMillis(30000));
-    }
-
-    @AfterSuite
-    public void tearDownAppium() {
-        driver.quit();
-    }
+public class LoginTest extends OurDriverManager {
 
     @Test
     public void ableToLogin() {
         Utils.sleep(2000);
         driver.activateApp("com.vendsy.tray.pos");
-        driver.rotate(ScreenOrientation.LANDSCAPE);
 
-
-        clickTrayImage().selectServer(hostName).clickLoginButton()
-                .loginToKDS(kdsUsername, kdsPassword).selectVenueInVenueList(venueName)
-                .selectDevice(kdsDevice);
+        clickTrayImage().selectServer(hostName).clickLoginButton().loginToKDS(kdsUsername, kdsPassword)
+                .selectVenueInVenueList(venueName).selectDevice(kdsDevice);
     }
 
-
     public LoginTest clickTrayImage() {
-        //wait.until(ExpectedConditions.elementToBeClickable(By.id("developer_mode")));
         MobileElement imgTray = driver.findElementById("developer_mode");
         for (int i = 1; i <= 5; i++)
             imgTray.click();
@@ -67,7 +25,6 @@ public class LoginTest {
     }
 
     public LoginTest selectServer(String serverName) {
-        //wait.until(ExpectedConditions.elementToBeClickable(By.id("android:id/text1")));
         MobileElement drpServers = driver.findElementById("android:id/text1");
         drpServers.click();
         MobileElement e = driver.findElementByXPath(SERVER_NAME.replace("<serverName>", serverName));
@@ -77,45 +34,33 @@ public class LoginTest {
 
 
     public LoginTest clickLoginButton() {
-        //wait.until(ExpectedConditions.elementToBeClickable(By.id("loginButton")));
         MobileElement btnLogin = driver.findElementById("loginButton");
         btnLogin.click();
         return this;
     }
 
     public LoginTest loginToKDS(String username, String password) {
-        // wait.until(ExpectedConditions.elementToBeClickable(By.id("submitButton")));
         MobileElement txtUsername = driver.findElementById("dialog_login_username");
         MobileElement txtPassword = driver.findElementById("dialog_login_password");
         MobileElement btnSubmit = driver.findElementById("submitButton");
         txtUsername.sendKeys(username);
         txtPassword.sendKeys(password);
-        Utils.sleep(10000);
         btnSubmit.click();
         return this;
     }
 
     public LoginTest selectVenueInVenueList(String venueName) {
-        boolean isDisplayed = false;
-        int counter = 0;
-
-        while (!isDisplayed || counter > 5) {
-            isDisplayed = (driver.findElementsByXPath(VENUE_NAME.replace("<venueName>", venueName)))
-                    .size() > 0 ? true : false;
-            Utils.sleep(20000);
-            counter++;
-        }
-
+        Utils.sleep(5000);
         MobileElement e = driver.findElementByXPath(VENUE_NAME.replace("<venueName>", venueName));
         e.click();
         return this;
     }
 
     public LoginTest selectDevice(String deviceName) {
-        Utils.sleep(10000);
-        WebElement selDevices = driver.findElementById("kdsList");
-        Select select = new Select(selDevices);
-        select.selectByVisibleText(deviceName);
+        Utils.sleep(5000);
+        WebElement selDevices = driver.findElementById("devicesListView");
+        MobileElement e = driver.findElementByXPath(DEVICE_NAME.replace("<deviceName>", deviceName));
+        e.click();
         return this;
     }
 
